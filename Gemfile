07348a4d273c31d910af1a82af8372d34bf73335
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2016-01-15 14:33:32 +0100

    add pagination to views

--- Gemfile
+++ Gemfile
@@ -8,6 +8,10 @@ gem 'bcrypt', '~> 3.1.10'
 
 # Bootstrap rails
 gem 'bootstrap-sass', '~> 3.3.6'
+# Pagination
+gem 'will_paginate', '~> 3.0.7'
+
+gem 'will_paginate-bootstrap', '~> 1.0.0'
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
@@ -45,8 +49,6 @@ group :development, :test do
   gem 'web-console', '~> 2.0'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring', '~> 1.6.2'
-  # debugger gem
-  gem 'byebug'
 end
 
 group :production do
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update, :show, :destroy]
 
   def index
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -28,7 +28,7 @@ class ArticlesController < ApplicationController
 
     # Create an article
     @article = Article.new(article_params)
-   @article.user = User.first
+    @article.user = User.first
     if @article.save
       flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
