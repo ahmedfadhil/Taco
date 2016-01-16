5a2a22086d904b4ee198251790f2e2b0497e853a
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2016-01-16 10:56:16 +0100

    add signin upon signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,9 @@ class UsersController < ApplicationController
     # Create user
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
